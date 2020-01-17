34641e3a07e6e1592c3a9439b845564f4a906ca2
Mikail Yildiz <iah71yildiz@gso-koeln.de>
2020-01-17 18:12:49 +0100

    Einen simplen Server programmiert
    Server antwortet auf eine Anfrage und gibt eine Antwort zurück.

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,13 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
 
 namespace Server
 {
+
+    // Für private Variablen wird ein "_" verwendet als Prefix
     class Program
     {
+
+        /// <summary>
+        /// InterNetwork = IPv4, Stream = Stream-oriented socket where both the sender and receiver can lump data in any size during sending or receiving of data
+        /// </summary>
+        private static Socket _serversocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static List<Socket> _clientSockets = new List<Socket>();
+        private static byte[] _buffer = new byte[1024];
+
+
         static void Main(string[] args)
         {
+            SetupServer();
+            Console.ReadLine();
         }
+
+        private static void SetupServer()
+        {
+            Console.WriteLine("Setting up server...");
+            _serversocket.Bind(new IPEndPoint(IPAddress.Any, 100));
+            _serversocket.Listen(1);
+            _serversocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket = _serversocket.EndAccept(AR);
+            _clientSockets.Add(socket);
+            Console.WriteLine("Client ist verbunden");
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            _serversocket.BeginAccept(new AsyncCallback(AcceptCallback), null); //Um weitere Verbindung aufbauen zu können
+
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            int received = socket.EndReceive(AR);
+            byte[] dataBuf = new byte[received];
+            Array.Copy(_buffer, dataBuf, received); // Inhalt des globalen Buffers in den lokalen Buffer schreiben, um anschließend Inhalt anzuzeigen
+
+            string text = Encoding.ASCII.GetString(dataBuf);
+            Console.WriteLine("Text empfangen: " + text);
+
+            string response = String.Empty;
+
+            if (text.ToLower() != "get time")
+            {
+                response = "ungültige Anfrage";
+            }
+            else
+            {
+                response = DateTime.Now.ToLongTimeString();
+            }
+            byte[] data = Encoding.ASCII.GetBytes(response);
+            socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+        }
+
+        private static void SendCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+        }
+
     }
 }
