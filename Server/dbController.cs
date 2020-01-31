3ba5c9e23edddcda08729b7be866c582eff7a51c
Mikail Yildiz <iah71yildiz@gso-koeln.de>
2020-01-31 18:08:14 +0100

    Datenbankverbindung kann hergestellt werden

---
+++ Server/dbController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Server
+{
+    static class dbController
+    {
+
+        internal static MySqlConnection con;  // wird verwendet, um eine Verbindung mit der Datenbank herzustellen.
+        internal static string connectionString;
+
+        static dbController()
+        {
+            //Datenbank Verbindung
+            connectionString = @"host=127.0.0.1;user=root;database=telefonico";
+            con = new MySqlConnection(connectionString);
+        }
+
+        // Um eine Verbindung mit der Datenbank herzustellen.
+        static internal void ConnectToLocalDb()
+        {
+            using (con)
+            {
+                try
+                {
+                    con.Open();
+
+                    //hierhin kommen weitere Befehle.
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+
+        //zum schliessen der Verbindung
+        static internal void DB_Disconnect()
+        {
+            con.Close();
+        }
+
+
+    }
+}
---
+++ datenbank/skelett.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE telefonico;
+
+create table user (
+ 
+  id int not null AUTO_INCREMENT,
+  email varchar(255),
+  password varchar(255),
+  
+  primary key (id)
+  
+  );
+  
+  
