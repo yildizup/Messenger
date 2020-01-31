bc5f210963c792aa5ad810d9248ad94fecad1fbe
Mikail Yildiz <iah71yildiz@gso-koeln.de>
2020-01-31 20:17:32 +0100

    Client kann sich registrieren
    Benutzerdaten werden in die SQL Datenbank hinzugefügt

---
+++ CommunicationHeader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class ComHeader
+{
+
+    /* Hier kommen die Header rein. Jeder Byte wird eine bestimmte Bedeutung haben. 
+     * Die Header werden verwendet, damit der Server die Pakete zuordnen kann */
+
+    public const byte hRegister = 0;     // Registrieren
+    public const string HALLO = "hallo"; //test
+
+
+}
