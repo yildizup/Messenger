78d745234d835a9cd7c3d1e7f74157c0ac02414b
Mikail Yildiz <iah71yildiz@gso-koeln.de>
2020-02-12 12:32:06 +0100

    Tabelle für Kontakte in die SQL Datenbank hinzugefügt

--- datenbank/skelett.sql
+++ datenbank/skelett.sql
@@ -2,17 +2,33 @@ CREATE DATABASE telefonico;
 
 create table user (
  
-  id int not null AUTO_INCREMENT,
   email varchar(255),
   password varchar(255),
   
-  primary key (id)
+  primary key (email)
   
   );
   
   create table contacts (
+	
+	main_email varchar(255),
+	friend_email varchar(255),
+	
+
+ foreign key (main_email)
+	references user(email),
+ foreign key (friend_email)
+	references user(email)
   
-  
-  
-  s
   ) ;
+  
+  
+ -- Die Benutzer
+insert into user (email, password) values ('absender@gmail.com','1');
+insert into user (email, password) values ('empfänger@gmail.com','1');
+insert into user (email, password) values ('nocheinfreund@gmail.com','1');
+
+
+-- Die Kontakte
+insert into contacts (main_email, friend_email) values ('absender@gmail.com','empfänger@gmail.com');
+insert into contacts (main_email, friend_email) values ('absender@gmail.com','nocheinfreund@gmail.com');
