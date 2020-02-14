4dfc4f664d0511c4d0ff5c459ed6a0453b04890d
Mikail Yildiz <iah71yildiz@gso-koeln.de>
2020-02-14 16:09:59 +0100

    Klassenbibliothek hinzugefügt

---
+++ SharedClass/SharedClasses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharedClass
+{
+    public class ContactList
+    {
+        List<string> listContacts = new List<string>();
+
+    }
+
+
+
+
+}
