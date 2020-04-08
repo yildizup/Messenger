54de5e7522001e74b4d0dee6220dd266c751c111
Mikail Yildiz <yildiz.m.mikail@gmail.com>
2020-04-08 21:39:15 +0200

    Oberfläche des Loginpage designed

--- Client/ViewModels/WindowViewModel.cs
+++ Client/ViewModels/WindowViewModel.cs
@@ -1,13 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows;
 
 namespace Client
 {
     public class WindowViewModel : ViewModelBase
     {
+
+        #region Private Variablen
+        /// <summary>
+        /// Der Window, der von diesem ViewModel kontrolliert wird
+        /// </summary>
+        private Window _Window;
+        #endregion
+
+        #region public Eigenschaften
+
+        #endregion
+
+
+        #region Konstruktoren
+        public WindowViewModel(Window window)
+        {
+            _Window = window;
+        }
+
+
+        #endregion
+
 
         /// <summary>
         /// Aktuelles "Page" der Anwendung
