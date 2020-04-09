edcfae94dc841b8c79f39c7b62a526b47dff26d4
Mikail Yildiz <yildiz.m.mikail@gmail.com>
2020-04-09 14:03:34 +0200

    Design Time Daten für die ChatList hinzugefügt

---
+++ Client/ViewModels/Chat/DesignModels/ChatListDesignModel.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace Client
+{
+
+    /// <summary>
+    /// Design-time Daten für <see cref="ChatListViewModel"/>
+    /// </summary>
+    public class ChatListDesignModel : ChatListViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// Eine einzige Instanz des Design Models
+        /// </summary>
+        public static ChatListDesignModel Instance => new ChatListDesignModel();
+        // Dasselbe wie: public static ChatListItemDesignModel Instance {get {return new ChatListItemDesignModel();}} C#7
+
+
+        #endregion
+
+        #region Konstruktor
+
+        public ChatListDesignModel()
+        {
+            Items = new List<ChatListItemViewModel>
+            {
+                new ChatListItemViewModel
+                {
+                    Name = "Cooper",
+                    Status = "Online"
+                },
+                new ChatListItemViewModel
+                {
+                    Name = "Morpheus",
+                    Status = "Offline"
+                },
+                new ChatListItemViewModel
+                {
+                    Name = "Mark",
+                    Status = "Offline"
+                },
+                new ChatListItemViewModel
+                {
+                    Name = "Salvadore",
+                    Status = "Offline"
+                }
+
+
+            };
+
+        }
+
+        #endregion
+    }
+}
---
+++ Client/ViewModels/ChatListViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Client
+{
+    /// <summary>
+    /// ein View Model für jeden individuellen "ChatListItem"
+    /// </summary>
+    public class ChatListViewModel : ViewModelBase
+    {
+
+        /// <summary>
+        /// Die Chat list items für die Liste
+        /// </summary>
+        public List<ChatListItemViewModel> Items { get; set; }
+
+
+
+    }
+}
