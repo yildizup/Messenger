b9f7d9641cf571342c2bc801313beb76d31a1006
Mikail Yildiz <yildiz.m.mikail@gmail.com>
2020-04-09 10:34:45 +0200

    ChatListItemControl hinzugefügt für die individuellen Benutzer

---
+++ Client/Controls/Chat/ChatListItemControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for ChatListItemControl.xaml
+    /// </summary>
+    public partial class ChatListItemControl : UserControl
+    {
+        public ChatListItemControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
